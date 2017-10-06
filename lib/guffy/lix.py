76b1cab378ef00fe7759b041b11b708b0aa6e2db
4hiziri <meirvg@gmail.com>
2017-10-06 22:51:31 +0900

    Refactoring, reduce temp_var.

--- lib/guffy/lix.py
+++ lib/guffy/lix.py
@@ -15,12 +15,13 @@ def extract_element(bs, tag, attr, class_=None):
     else:
         page = bs.find_all(tag, class_=class_)
 
-        has_attr = filter(lambda x: attr in x.attrs, page)
-        return list(map(lambda x: x.attrs[attr], has_attr))
+    return list(map(lambda x: x.attrs[attr],
+                    filter(lambda x: attr in x.attrs, page)))
 
 
 def extract_link(bsobj, class_=None, permit_other_host=False):
     """This extract links in href of a-tag.
+    NOT IMPLEMENT!
     If permit_other_host is True,
     this returns links including extern host's address"""
     # get all links in page.
