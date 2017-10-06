5c87cfc5fae859265f6eca5874de0ddb7c670e63
4hiziri <meirvg@gmail.com>
2017-10-06 22:51:41 +0900

    Add test

---
+++ lib/tests/test_crawlutil.py
@@ -0,0 +1,47 @@
+from guffy import crawlutil as crawl
+import unittest
+from bs4 import BeautifulSoup
+
+
+class TestCrawlUtil(unittest.TestCase):
+    html = '''<!DOCTYPE html>
+<html class="client-js ve-not-available" dir="ltr" lang="ja">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <title>web page for test</title>
+    <a href='/index.php'>test link</a>
+    <a href='this/is/test'></a>
+  </head>
+</html>
+'''
+
+    bsObj = BeautifulSoup(html, "lxml")
+
+    def test_find_all_urls(self):
+        self.assertListEqual(
+            crawl.find_all_urls(self.bsObj), ['/index.php', 'this/is/test'])
+
+    def test_abs_path(self):
+        self.assertEqual(
+            crawl.abs_path('http://www.example.com', '/index.php'),
+            'http://www.example.com/index.php')
+        self.assertEqual(
+            crawl.abs_path('http://www.example.com/under1/', 'under2.txt'),
+            'http://www.example.com/under1/under2.txt')
+
+    def test_search_urls(self):
+        self.assertListEqual(
+            crawl.search_urls([
+                'https://www.example.com/test.txt',
+                'https://www.example.com/test/test.jpg',
+                'https://www.example.com/test/test1.jpg',
+                'https://www.example.com/test/test02.jpg'
+            ], r'.*test[0-9]+.jpg'), [
+                'https://www.example.com/test/test1.jpg',
+                'https://www.example.com/test/test02.jpg'
+            ])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ lib/tests/test_lix.py
@@ -0,0 +1,45 @@
+import unittest
+from guffy import lix
+import os
+import sys
+
+
+class TestLix(unittest.TestCase):
+    from bs4 import BeautifulSoup
+    html = '''<!DOCTYPE html>
+<html class="client-js ve-not-available" dir="ltr" lang="ja">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <title>web page for test</title>
+    <a href='/index.php'>test link</a>
+    <a href='this/is/test'></a>
+    <img src='img/testsource'>
+  </head>
+</html>
+'''
+
+    bsObj = BeautifulSoup(html, "lxml")
+
+    def test_extract_element(self):        
+        self.assertListEqual(
+            lix.extract_element(self.bsObj, 'a', 'href'),
+            ['/index.php', 'this/is/test'])
+        self.assertListEqual(
+            lix.extract_element(self.bsObj, 'img', 'src'), ['img/testsource'])
+
+    def test_extract_link(self):
+        self.assertListEqual(
+            lix.extract_element(self.bsObj, 'a', 'href'),
+            lix.extract_link(self.bsObj))
+
+    def test_extract_link_by_query(self):
+        self.assertListEqual(
+            lix.extract_link_by_query(self.bsObj, '.*\.php'), ['/index.php'])
+
+    def test_extract_img(self):
+        self.assertListEqual(lix.extract_img(self.bsObj), ['img/testsource'])
+
+
+if __name__ == '__main__':
+    unittest.main()
